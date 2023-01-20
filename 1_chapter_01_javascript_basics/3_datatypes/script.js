b1af5841d9a29e8d1836ad6f0ec917e3f1f0a870
Shubham Vishwakarma <shubham@itsbhm.group>
2023-01-20 09:09:19 +0530

    DataTypes File Updated

--- 1_chapter_01_javascript_basics/3_datatypes/script.js
+++ 1_chapter_01_javascript_basics/3_datatypes/script.js
@@ -2,3 +2,35 @@
 Primitive data types are number, string, boolean, NULL, Infinity and symbol.
 Non-primitive data types is the object. The JavaScript arrays and functions are also objects.
 */
+
+/*
+1. Number. Floating point numbers are Used for decimals and integers
+// let age = 23;
+
+2. String: Sequence of characters are Used for text
+// let firstName = "Customer"
+
+3. Boolean: Logical type that can only be true or false and Used for taking decisions
+// let fullAge = false;
+
+4. Undefined: Value taken by a variable that is not yet defined ('empty value')
+// let children;
+
+5. Null: Also means 'empty value'
+
+6. Symbol (ES2015): Value that is unique and cannot be changed [Not useful for now]
+
+7. Bigint (ES2020): Larger integers than the Number type can hold
+*/
+
+// Define some variables
+
+var fullName = "Nirmal Jyoti";
+var age = 28;
+var isComesInSeniorCategory = false;
+var salary = 32000.98;
+
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof isComesInSeniorCategory);
+console.log(typeof salary);
