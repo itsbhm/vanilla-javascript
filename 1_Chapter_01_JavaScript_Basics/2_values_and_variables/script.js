82b4615d3b5eb0ea30317a2f0a88470ce2c05daa
Shubham Vishwakarma <shubham@itsbhm.group>
2023-01-20 08:18:52 +0530

    Values and Variables Files Added

---
+++ 1_Chapter_01_JavaScript_Basics/2_values_and_variables/script.js
@@ -0,0 +1,24 @@
+/*
+The general rules for constructing names for variables (unique identifiers) are:
+
+1). Names can contain letters, digits, underscores, and dollar signs.
+2). Names must begin with a letter.
+3). Names can also begin with $.
+4). Names are case sensitive (y and Y are different variables).
+5). Reserved words (like JavaScript keywords) cannot be used as names.
+
+*/
+
+// Basics of Declearing Variables
+var jsNote = "JavaScript is Scripting lang";
+console.log(jsNote);
+
+
+// Variables (Camel Case)
+var firstName = "Taniya";
+var rollNo = 1234;
+var _collegeCode = 1029;
+
+console.log(firstName);
+console.log(rollNo);
+console.log(_collegeCode);
